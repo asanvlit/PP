b6483491b97fcfcea88bc47e072321c710a22866
asanvlit <asanvlit@gmail.com>
2022-11-01 22:26:28 +0300

    fix task 4

--- mpi/task/block2/block2Tasks.cpp
+++ mpi/task/block2/block2Tasks.cpp
@@ -7,6 +7,15 @@
 
 int rank, size;
 
+void print_matrix(double **m, int rows, int cols) {
+    for (int i = 0; i < rows; ++i) {
+        std::cout << i << ": ";
+        for (size_t j = 0; j < cols; ++j)
+            std::cout << m[i][j] << '\t';
+        std::cout << std::endl;
+    }
+}
+
 void task1() {
     const int n = 100;
 
@@ -251,7 +260,7 @@ void task3() {
             printf("\n");
         }
     }
-    else if (rank != 0) {
+    else {
         int count, start;
         double batchA[n][n], batchB[n][n], batchC[n];
 
@@ -276,10 +285,10 @@ void task3() {
 }
 
 int task4() {
-    int aColumn = 4;
-    int aRow = 4;
-    int bColumn = 4;
-    int bRow = 4;
+    int aColumn = 2;
+    int aRow = 3;
+    int bColumn = 3;
+    int bRow = 2;
 
     if (aColumn != bRow) {
         return -1;
@@ -287,156 +296,107 @@ int task4() {
 
     double a[aRow][aColumn], b[bRow][bColumn], c[aRow][bColumn];
 
+    int batch = ceil((double) aRow / (size - 1));
+
     if (rank == 0) {
-        srand(static_cast<unsigned int>(time(0)));
+//        srand(static_cast<unsigned int>(time(0)));
         for (int i = 0; i < aRow; i++) {
             for (int j = 0; j < aColumn; j++) {
-                a[i][j] = rand() % 20;
+                a[i][j] = rand() % 10;
             }
         }
         for (int i = 0; i < bRow; i++) {
             for (int j = 0; j < bColumn; j++) {
-                b[i][j] = rand() % 20;
+                b[i][j] = rand() % 10;
             }
         }
 
-        int row, column = 0;
         printf("Matrix A:\n");
-        for (row = 0; row < aRow; row++) {
-            for (column = 0; column < aColumn; column++) {
+        for (int row = 0; row < aRow; row++) {
+            for (int column = 0; column < aColumn; column++) {
                 printf("%f     ", a[row][column]);
             }
             printf("\n");
         }
-        row, column = 0;
         printf("Matrix B:\n");
-        for (row = 0; row < bRow; row++) {
-            for (column = 0; column < bColumn; column++) {
+        for (int row = 0; row < bRow; row++) {
+            for (int column = 0; column < bColumn; column++) {
                 printf("%f     ", b[row][column]);
             }
             printf("\n");
         }
 
-        double* lineA = new double[aRow * aColumn];
-        double* lineB = new double[bRow * bColumn];
+        double transportedB[bColumn][bRow];
 
         for (int i = 0; i < aRow; i++) {
             for (int j = 0; j < aColumn; j++) {
-                // каким образом осуществляется преобразование
-                lineA[i * aColumn + j] = a[i][j];
-            }
-        }
-        for (int i = 0; i < bRow; i++) {
-            for (int j = 0; j < bColumn; j++) {
-                lineB[i * bColumn + j] = b[i][j];
+                transportedB[i][j] = b[j][i];
             }
         }
 
         for (int i = 1; i < size; i++) {
-            MPI_Send(lineA, aRow * aColumn, MPI_DOUBLE, i, 99, MPI_COMM_WORLD);
-            MPI_Send(lineB, bRow * bColumn, MPI_DOUBLE, i, 100, MPI_COMM_WORLD);
-        }
+            int rowStartNumber = (i - 1) * batch;
 
-        for (int i = 1; i < size; i++) {
-            MPI_Status status;
-
-            int batch = aRow / (size - 1) + 1;
-            int start = (i - 1) * batch;
-            int end = i * batch;
-
-            // если вышли за границу, то границей станет последний элемент строки
-            if (end > aRow) {
-                end = aRow;
-            }
-            if (start > aRow) {
-                start = aRow;
-            }
-            if (start == end) {
-                continue;
+            if (aRow - rowStartNumber <= batch) {
+                batch = aRow - rowStartNumber;
             }
 
-            double* batchC = new double[(end - start) * bColumn];
+            // чтобы понять сколько раз должен быть send в потоках где rank != 0
+            MPI_Send(&batch, 1, MPI_INT, i, 99, MPI_COMM_WORLD);
+            MPI_Send(&rowStartNumber, 1, MPI_INT, i, 100, MPI_COMM_WORLD);
 
-            MPI_Recv(batchC, (end - start) * bColumn, MPI_DOUBLE, i, 101, MPI_COMM_WORLD, &status);
-            for (int j = start; j < end; j++) {
+            for (int j = rowStartNumber; j < rowStartNumber + batch; j++) {
                 for (int k = 0; k < bColumn; k++) {
-                    c[j][k] = batchC[(j - start) * bColumn + k];
+                    MPI_Send(&a[j][0], aColumn, MPI_DOUBLE, i, 101, MPI_COMM_WORLD);
+                    MPI_Send(&transportedB[k][0], bRow, MPI_DOUBLE, i, 102, MPI_COMM_WORLD);
+                }
+            }
+
+            for (int j = rowStartNumber; j < rowStartNumber + batch; j++) {
+                for (int k = 0; k < bColumn; k++) {
+                    double *batchC = new double[1];
+                    MPI_Status status;
+
+                    MPI_Recv(batchC, 1, MPI_DOUBLE, i, 103, MPI_COMM_WORLD, &status);
+                    c[j][k] = batchC[0];
                 }
             }
         }
-        row, column = 0;
+
         printf("Matrix C:\n");
-        for (row = 0; row < aRow; row++) {
-            for (column = 0; column < bColumn; column++) {
+        for (int row = 0; row < aRow; row++) {
+            for (int column = 0; column < bColumn; column++) {
                 printf("%f     ", c[row][column]);
             }
             printf("\n");
         }
-    }
-    else {
-        int count;
+    } else {
+        int count, start;
+        double a1[aRow], b1[bRow];
+        double *batchC = new double[1];
 
-        MPI_Status status;
-        MPI_Probe(0, 99, MPI_COMM_WORLD, &status);
-        MPI_Get_count(&status, MPI_DOUBLE, &count);
-        double* batchA = new double[count];
-        MPI_Recv(batchA, count, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD, &status);
+        MPI_Status statusA;
+        MPI_Status statusB;
+        MPI_Status statusElem;
+        MPI_Status statusStart;
 
-        double a1[aRow][aColumn];
-        double b1[bRow][bColumn];
+        MPI_Recv(&count, 1, MPI_INT, MPI_ANY_SOURCE, 99,MPI_COMM_WORLD, &statusElem);
+        MPI_Recv(&start, 1, MPI_INT, MPI_ANY_SOURCE, 100, MPI_COMM_WORLD,&statusStart);
 
-        aColumn = count / aRow;
-        for (int i = 0; i < aRow; i++) {
-            for (int j = 0; j < aColumn; j++) {
-                a1[i][j] = batchA[i * aColumn + j];
-            }
-        }
+        for (int i = start; i < start + count; i++) {
+            for (int k = 0; k < bColumn; k++) {
+                batchC[0] = 0.0;
 
-        MPI_Probe(0, 100, MPI_COMM_WORLD, &status);
-        MPI_Get_count(&status, MPI_DOUBLE, &count);
-        double* batchB = new double[count];
-        MPI_Recv(batchB, count, MPI_DOUBLE, 0, 100, MPI_COMM_WORLD, &status);
+                MPI_Recv(&a1, aColumn, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD, &statusA);
+                MPI_Recv(&b1, bRow, MPI_DOUBLE, 0, 102, MPI_COMM_WORLD, &statusB);
 
-        bColumn = count / bRow;
-        for (int i = 0; i < bRow; i++) {
-            for (int j = 0; j < bColumn; j++) {
-                b1[i][j] = batchB[i * bColumn + j];
-            }
-        }
-
-        int batch = aRow / (size - 1) + 1;
-        int start = (rank - 1) * batch; // определяем какой диапазон будем рассматривать для строк матрицы
-        int end = rank * batch;
-
-        if (end > aRow) {
-            end = aRow;
-        }
-        if (start > aRow) {
-            start = aRow;
-        }
-        if (start == end) {
-            return 0;
-        }
-
-        double batchC[end - start][bColumn];
-
-        for (int i = start; i < end; i++) {
-            for (int j = 0; j < bColumn; j++) {
-                double sum = 0;
-                for (int k = 0; k < aColumn; k++) {
-                    sum += a1[i][k] * b1[k][j];
+                for (int j = 0; j < aRow; j++) {
+                    batchC[0] += a1[j] * b1[j];
                 }
-                batchC[i - start][j] = sum;
-            }
-        }
 
-        double resultC[(end - start) * bColumn];
-        for (int i = 0; i < end - start; i++) {
-            for (int j = 0; j < bColumn; j++) {
-                resultC[i * bColumn + j] = batchC[i][j];
+                MPI_Send(batchC, 1, MPI_DOUBLE, 0, 103, MPI_COMM_WORLD);
             }
         }
-        MPI_Send(resultC, (end - start) * bColumn, MPI_DOUBLE, 0, 101,MPI_COMM_WORLD);
     }
 
     return 0;
@@ -547,7 +507,11 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    task3();
+    task4();
+//    double starttime, endtime;
+//    starttime = MPI_Wtime();
+//    endtime = MPI_Wtime();
+//    printf("Time:  %f s\n",endtime-starttime);
     MPI_Finalize();
     return 0;
 }
